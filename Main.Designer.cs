483624fee00fd0a0c8f41efdb1b5cbd8e46f0be0
Hasan-Hüseyin-Karakaya <hasankarakaya0341@gmail.com>
2024-06-17 18:36:14 +0300

    Publish

---
+++ Main.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TextToBinary
+{
+    partial class Main
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.RichTextBox();
+            this.textBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonToggle = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.textBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInput.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.textBoxInput.Location = new System.Drawing.Point(16, 52);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(186, 246);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.Text = "";
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.textBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxOutput.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.textBoxOutput.Location = new System.Drawing.Point(263, 52);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(186, 246);
+            this.textBoxOutput.TabIndex = 1;
+            this.textBoxOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.label2.Location = new System.Drawing.Point(259, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Binary";
+            // 
+            // buttonToggle
+            // 
+            this.buttonToggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonToggle.Font = new System.Drawing.Font("Quicksand Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonToggle.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.buttonToggle.Location = new System.Drawing.Point(12, 360);
+            this.buttonToggle.Name = "buttonToggle";
+            this.buttonToggle.Size = new System.Drawing.Size(437, 37);
+            this.buttonToggle.TabIndex = 5;
+            this.buttonToggle.Text = "Convert to Binary";
+            this.buttonToggle.UseVisualStyleBackColor = true;
+            this.buttonToggle.Click += new System.EventHandler(this.buttonToggle_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.checkBox1.Location = new System.Drawing.Point(12, 317);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(166, 28);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Copy to Clipboard";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(461, 409);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonToggle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Font = new System.Drawing.Font("Quicksand Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text To Binary / Binary To Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBoxInput;
+        private System.Windows.Forms.RichTextBox textBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonToggle;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TextToBinary
+{
+    public partial class Main : Form
+    {
+        private bool isTextToBinary = true; // Current conversion mode (Text to Binary or Binary to Text)
+        private string binaryOutput = string.Empty; // Stores binary output
+        private string textOutput = string.Empty; // Stores text output
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        // Function to convert Text to Binary
+        private string ConvertTextToBinary(string text)
+        {
+            string binaryString = "";
+            foreach (char c in text)
+            {
+                // Converts each character to an 8-bit binary format and separates with a space
+                binaryString += Convert.ToString(c, 2).PadLeft(8, '0') + " ";
+            }
+            return binaryString.Trim(); // Removes trailing space and returns
+        }
+
+        // Function to convert Binary to Text
+        private string ConvertBinaryToText(string binary)
+        {
+            string[] binaryArray = binary.Split(' ');
+            StringBuilder text = new StringBuilder();
+
+            foreach (string binaryChar in binaryArray)
+            {
+                if (!string.IsNullOrEmpty(binaryChar))
+                {
+                    // Converts each binary character to integer and then to ASCII character
+                    text.Append((char)Convert.ToUInt64(binaryChar, 2));
+                }
+            }
+            return text.ToString(); // Returns the resulting text
+        }
+
+        // Triggered when text input changes
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            if (isTextToBinary)
+            {
+                // If in Text to Binary conversion mode
+                string inputText = textBoxInput.Text;
+                binaryOutput = ConvertTextToBinary(inputText); // Converts text to binary
+                textBoxOutput.Text = binaryOutput; // Shows binary output
+
+                // If 'Copy' option is checked and output is not empty, copy to clipboard
+                if (binaryOutput != string.Empty && checkBox1.Checked)
+                {
+                    Clipboard.SetText(binaryOutput);
+                }
+            }
+            else
+            {
+                // If in Binary to Text conversion mode
+                if (IsValidBinaryInput(textBoxInput.Text))
+                {
+                    string binaryText = textBoxInput.Text;
+                    textOutput = ConvertBinaryToText(binaryText); // Converts binary to text
+                    textBoxOutput.Text = textOutput; // Shows text output
+
+                    // If 'Copy' option is checked and output is not empty, copy to clipboard
+                    if (textOutput != string.Empty && checkBox1.Checked)
+                    {
+                        Clipboard.SetText(textOutput);
+                    }
+                }
+                else
+                {
+                    // Removes invalid binary characters
+                    textBoxInput.Text = RemoveInvalidBinaryCharacters(textBoxInput.Text);
+                }
+            }
+        }
+
+        // Button function to toggle conversion mode
+        private void buttonToggle_Click(object sender, EventArgs e)
+        {
+            // Clears input and output boxes
+            textBoxInput.Text = string.Empty;
+            textBoxOutput.Text = string.Empty;
+
+            // Updates mode and labels
+            if (isTextToBinary)
+            {
+                label1.Text = "Binary";
+                label2.Text = "Text";
+                buttonToggle.Text = "Convert to Text";
+            }
+            else
+            {
+                label1.Text = "Text";
+                label2.Text = "Binary";
+                buttonToggle.Text = "Convert to Binary";
+            }
+            isTextToBinary = !isTextToBinary; // Toggles the mode
+        }
+
+        // Function to check valid binary input
+        private bool IsValidBinaryInput(string input)
+        {
+            foreach (char c in input)
+            {
+                if (c != '0' && c != '1' && c != ' ')
+                {
+                    return false; // Returns false if there are invalid characters
+                }
+            }
+            return true; // Returns true if input is valid binary
+        }
+
+        // Function to remove invalid binary characters
+        private string RemoveInvalidBinaryCharacters(string input)
+        {
+            StringBuilder validInput = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (c == '0' || c == '1' || c == ' ')
+                {
+                    validInput.Append(c); // Appends valid characters
+                }
+            }
+            return validInput.ToString(); // Returns cleaned input
+        }
+
+        // Checkbox change event for 'Copy' functionality
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                // If 'Copy' option is checked
+                if (isTextToBinary)
+                {
+                    if (binaryOutput != string.Empty)
+                        Clipboard.SetText(binaryOutput); // Copies binary output to clipboard
+                }
+                else
+                {
+                    if (textOutput != string.Empty)
+                        Clipboard.SetText(textOutput); // Copies text output to clipboard
+                }
+            }
+        }
+    }
+}
